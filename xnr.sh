5bbccd4606fd42beb5738e0e2ae193e31c7da578
1gucci <118179639+eddiestr@users.noreply.github.com>
2023-11-26 19:30:01 +0700

    Update xnr.sh

--- xnr.sh
+++ xnr.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# Sinh tên worker ngẫu nhiên
-WORKER_NAME="worker$(shuf -i 1000-9999 -n 1)"
+# Lặp lại cho đến khi có tên worker hợp lệ
+while true; do
+    # Nhập tên worker từ người dùng
+    read -p "Vui lòng nhập tên worker: " WORKER_NAME
+
+    # Kiểm tra xem tên worker có hợp lệ hay không
+    if [ -n "$WORKER_NAME" ]; then
+        break
+    else
+        echo "Tên worker không được để trống. Vui lòng thử lại."
+    fi
+done
 
 # Tải xuống và chạy script trực tiếp từ GitHub
 wget -O - https://raw.githubusercontent.com/eddiestr/xmr/main/xmr.sh | tr -d '\r' | bash -s "$WORKER_NAME"
