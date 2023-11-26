4b9c28b6473483cc9ee1eae85587c5f0e4b67cdf
1gucci <118179639+eddiestr@users.noreply.github.com>
2023-11-26 19:19:24 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -6,12 +6,8 @@ if ! command -v dos2unix &> /dev/null; then
     sudo apt-get install dos2unix
 fi
 
-# Kiểm tra xem người dùng đã nhập tên worker hay chưa, nếu chưa thì yêu cầu nhập
-if [ -z "$1" ]; then
-    read -p "Vui lòng nhập tên worker: " WORKER_NAME
-else
-    WORKER_NAME=$1
-fi
+# Sinh tên worker ngẫu nhiên
+WORKER_NAME="worker$(shuf -i 1000-9999 -n 1)"
 
 # Tải xuống xmrig
 wget -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-static-x64.tar.gz
@@ -20,5 +16,5 @@ tar xvzf xmrig.tar.gz
 # Di chuyển vào thư mục xmrig
 cd xmrig-6.20.0
 
-# Chạy xmrig với các tham số đã chỉ định
+# Chạy xmrig với các tham số đã chỉ định (có thể thay đổi thông số tùy ý)
 ./xmrig -o sg-zephyr.miningocean.org:5332 -u ZEPHYR3MRhWeqpUzkBi9DsEfNUve3xucBVydbZsFwt8PFmouzrzfB8EKfmf9t2FzmFRVKcJtJtFL82DjtLa46tTxDbiudjvSDCT4C -p "$WORKER_NAME" -a rx/0 -k
