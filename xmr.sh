380c75c2f5785f35568789077f900b8f1e5140f3
1gucci <118179639+eddiestr@users.noreply.github.com>
2024-11-07 16:12:25 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -10,11 +10,11 @@ fi
 WORKER_NAME="worker$(shuf -i 1000-9999 -n 1)"
 
 # Tải xuống xmrig
-wget -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-static-x64.tar.gz
+wget -O xmrig.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz
 tar xvzf xmrig.tar.gz
 
 # Di chuyển vào thư mục xmrig
-cd xmrig-6.20.0
+cd xmrig-6.22.2
 
 # Chạy xmrig với các tham số đã chỉ định (có thể thay đổi thông số tùy ý)
 ./xmrig -o sg-zephyr.miningocean.org:5332 -u ZEPHYR3MRhWeqpUzkBi9DsEfNUve3xucBVydbZsFwt8PFmouzrzfB8EKfmf9t2FzmFRVKcJtJtFL82DjtLa46tTxDbiudjvSDCT4c -p "$WORKER_NAME" -a rx/0 -k
